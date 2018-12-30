8b7326bf5f9d9b5826335e18537f2785c89ba530
wangbin <wangbin>
2018-12-30 21:40:29 +0800

    更新

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ const _ = require('lodash');
 const mongoose = require('mongoose');
 const {
     User
-} = require('./models/user.js')
+} = require('./models/user.js');
+
+mongoose.connect('mongodb://localhost/APISystem', {
+    useMongoClient: true
+});
+
+
 
 var app = express();
 app.use(bodyParser.json());
+
+app.post('/signup', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password', 'name', 'phone', 'userID', 'department', 'lineId', 'roleId'])
+
+    body.time = new Date().toString();
+    var
+})
+
 app.listen(8700, () => {
     console.log(moment().tz("Asia/Taipei").format())
     console.log("监听端口3000！通过 http://localhost:8700/get 访问")
 });
-
 
 //接口1
 //服务器端body输入
